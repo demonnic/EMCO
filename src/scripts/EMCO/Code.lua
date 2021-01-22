dbd21dfcf47361f0c3b5deccdacdc1c947b77c9f
Damian Monogue <demonnic@gmail.com>
2021-01-22 18:54:08 -0500

    Uncomplicate require for EMCO now the version isn't interfering

--- src/scripts/EMCO/Code.lua
+++ src/scripts/EMCO/Code.lua
@@ -1,12 +1,5 @@
 if not EMCO then
-	local path = package.path
-	local home_dir = getMudletHomeDir() .. "/@PKGNAME@"
-	local lua_dir = string.format( "%s/%s", home_dir, [[?.lua]] )
-	local init_dir = string.format( "%s/%s", home_dir, [[?/init.lua]] )
-	package.path = string.format( "%s;%s;%s", path, lua_dir, init_dir )
-
-	local okay, content = pcall( require, "EMCO" )
-	package.path = path
+	local okay, content = pcall( require, "EMCO.EMCO" )
 	if okay then
 		EMCO = content
 	else
